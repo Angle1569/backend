e8bcb53174fe8113fe6dbecc062dedaa4de7bab0
it3suman <halwai1569@gmail.com>
2025-06-23 13:59:08 +0530

    monogo url

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const Router = require("./project/routes/auth");
-const URI = process.env.URI || "mongodb://localhost:27017/mydatabase";
+const URI = process.env.MONGO_URL || "mongodb://localhost:27017/mydatabase";
 const PORT = process.env.PORT || 3000;
 const productRoutes = require("./project/routes/product");
 const saleBillRoutes = require("./project/routes/saleBill");
@@ -26,12 +26,12 @@ server.use(express.json());
 mongoose.Promise = global.Promise;
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(URI, {
+  .connect(MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(console.log("Connected to database"))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err), process.exit(1));
 
 Router(server);
 
