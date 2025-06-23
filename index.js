74bd4df1d5b19a486618a1992439a17afec9b79d
it3suman <halwai1569@gmail.com>
2025-06-23 15:15:27 +0530

    url udate

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const Router = require("./project/routes/auth");
-const URI = process.env.MONGO_URL || "mongodb://localhost:27017/mydatabase";
+// const URI = process.env.MONGO_URL || "mongodb://localhost:27017/mydatabase";
 const PORT = process.env.PORT || 3000;
 const productRoutes = require("./project/routes/product");
 const saleBillRoutes = require("./project/routes/saleBill");
@@ -26,7 +26,7 @@ server.use(express.json());
 mongoose.Promise = global.Promise;
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(URI, {
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
